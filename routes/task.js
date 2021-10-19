9de9aff875eacb8b1633409acacd03d0aa851864
JustinAMB <Justinmb@outlook.es>
2021-10-19 14:32:18 -0600

    se crea la ruta de eliminar task

--- routes/task.js
+++ routes/task.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const { listaTasks, crearTask, cambiarEstadoTask } = require('../controllers/task');
+const { listaTasks, crearTask, cambiarEstadoTask, eliminarTask } = require('../controllers/task');
 router.post('/create', [check('descripcion', 'la descripcion es obligatoria').not().isEmpty(),
     validarCampos
 ], crearTask);
 router.get('/:project', listaTasks);
 
 router.put('/:id', cambiarEstadoTask);
+router.delete('/:id', eliminarTask);
 module.exports = router;
