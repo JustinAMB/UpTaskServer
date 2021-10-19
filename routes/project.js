984228adb5a892f75ae196a12742d30fe271ffda
JustinAMB <Justinmb@outlook.es>
2021-10-19 01:00:33 -0600

    se implementan las validaciones en la ruta de project

--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { listaProjects, crearProject } = require('../controllers/project');
-router.post('/create', crearProject);
+
+router.post('/create', [check('titulo', 'el titulo es obligatorio').not().isEmpty(),
+    validarCampos
+], crearProject);
 router.get('/:user', listaProjects);
 module.exports = router;
