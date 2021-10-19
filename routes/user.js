109c2717c9ace0d21ff08df38b4bb126456f53f0
JustinAMB <Justinmb@outlook.es>
2021-10-19 00:55:27 -0600

    se implementan las validaciones en la ruta de user

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const { loginUser, registroUser, revalidarToken } = require('../controllers/user');
-router.post('/registro', registroUser);
-router.post('/login', loginUser);
+router.post('/registro', [check('username', 'El nombre de usuario es obligatorio').not().isEmpty(), check('email', 'el email es obligatorio').isEmail(), check('password', 'la contraseña es obligatoria').isLength({ min: 6 }),
+    validarCampos
+], registroUser);
+router.post('/login', [check('email', 'el email es obligatorio').isEmail(), check('password', 'la contraseña es obligatoria').isLength({ min: 6 }),
+    validarCampos
+], loginUser);
 
 router.get('/renew', validarJWT, revalidarToken);
 module.exports = router;
