f523f723ddc1d5b74238131fa4901307b6a7c117
JustinAMB <Justinmb@outlook.es>
2021-10-19 00:59:10 -0600

    se implementan las validaciones en la ruta de task

--- routes/task.js
+++ routes/task.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
+const { validarCampos } = require('../middlewares/validar-campos');
 const { listaTasks, crearTask } = require('../controllers/task');
-router.post('/create', crearTask);
+router.post('/create', [check('descripcion', 'la descripcion es obligatoria').not().isEmpty(),
+    validarCampos
+], crearTask);
 router.get('/:project', listaTasks);
 module.exports = router;
